2203c5a07021f72d8a56f206cec9953de36fb4e2
kaller01 <martin@kallers.se>
2023-01-01 03:10:07 +0100

    multiplayer support

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-const express = require('express')
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 const path = require('path');
-const app = express()
 const port = 3090
+
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/index.html'));
@@ -9,6 +12,38 @@ app.get('/', (req, res) => {
 
 app.use(express.static('public'))
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+http.listen(port, () => console.log(`Listening on port ${port}`));
+
+const idmap = {};
+
+const players = {};
+
+io.on('connection', (socket) => {
+  console.log('a user connected', socket.id);
+  idmap[socket.id] = Math.floor(Math.random() * 4294967295);
+  players[idmap[socket.id]] = {
+    x: 0,
+    y: 0,
+    color: {
+      r: 0,
+      g: 0,
+      b: 0
+    }
+  }
+
+  socket.on('player_pos', (data) => {
+    delete players[idmap[socket.id]];
+    socket.emit('players_update', players);
+    players[idmap[socket.id]] = {
+      x: data.x,
+      y: data.y,
+      color: data.color
+    }
+  });
+
+  socket.on('disconnect', () => {
+    delete players[idmap[socket.id]];
+    delete idmap[socket.id];
+  });
+
+});
